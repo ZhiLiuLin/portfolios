corrected returns calculation to account for shorting, allowed shorted Markowitz portfolios, and tided returns calculation

base.py
@@ -59,14 +59,23 @@ class DynamicPortfolio(FixedPortfolio):
 		return self._weights.loc[date]
 
 	def get_returns(self, start=None, end=None):
-		if start:
-			if not end:
-				end = start
-				start = self._quotes.index[self._quotes.index.get_loc(start)-1]	
-			pure_returns = self._quotes.loc[start:end].fillna(0).pct_change()
+		if not start and not end:
+			start = self._weights.index[0]
+			end = self._weights.index[-1]
+		elif start and end:
+			if start <= self._weights.index[1]:
+				start = self._weights.index[0]
+			else:
+				start = self._weights.index[self._weights.index.searchsorted(start)-1]
 		else:
-			pure_returns = self._quotes.fillna(0).pct_change()
-		return (pure_returns * pd.DataFrame(pure_returns.index.map(self.get_weights).tolist(), index=pure_returns.index).shift(1)).sum(axis=1)[1:].rename(self.name)
+			i = self._weights.index.searchsorted(start)
+			start = self._weights.index[i-1]
+			end = self._weights.index[i]
+
+		pure_returns = self._quotes.loc[start:end].fillna(0).pct_change()
+		pure_returns.index.map(self.get_weights)
+		weights = self._weights.loc[pure_returns.index].shift(1)
+		return (pure_returns * weights).sum(axis=1)[1:].rename(self.name) / weights[weights > 0].iloc[1:].sum(axis=1)
 
 
 class RandomlyWeightedPortfolio(DynamicPortfolio):

markowitz.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-from qpsolvers import solve_qp
-from scipy import sparse
+# from qpsolvers import solve_qp
+# from scipy import sparse
 
 from .base import DynamicPortfolio
 
@@ -17,30 +17,21 @@ def gmv(covariance: np.ndarray):
 	-------
 	Vector
 	"""
-	# n = covariance.shape[0]
-	# M = np.append( np.append(covariance, np.ones((1, n)), axis=0), -np.ones((n+1, 1)), axis=1 )
-	# M[n, n] = 0
+	n = covariance.shape[0]
+	M = np.append( np.append(covariance, np.ones((1, n)), axis=0), -np.ones((n+1, 1)), axis=1 )
+	M[n, n] = 0
 
-	# v = np.zeros(n+1)
-	# v[n] = 1
-	# current = np.linalg.solve(M, v)[:-1]
+	v = np.zeros(n+1)
+	v[n] = 1
+	return np.linalg.solve(M, v)[:-1]
 
-	# while any(current < 0):
-	# 	for i, w in enumerate(current):
-	# 		if w < 0:
-	# 			M[i, :] = 0
-	# 			M[:, i] = 0
-	# 			M[i, i] = 1
-	# 	current = np.linalg.solve(M, v)[:-1]
-	# return current
-
-	return solve_qp(sparse.csc_matrix(covariance),
-					q=np.zeros( (n:=covariance.shape[0]) ), 
-					A=sparse.csc_matrix(np.ones(n)), 
-					b=np.ones(1), 
-					lb=np.zeros(n), 
-					ub=np.ones(n), 
-					solver="clarabel")
+	# return solve_qp(sparse.csc_matrix(covariance),
+	# 				q=np.zeros( (n:=covariance.shape[0]) ), 
+	# 				A=sparse.csc_matrix(np.ones(n)), 
+	# 				b=np.ones(1), 
+	# 				lb=np.zeros(n), 
+	# 				ub=np.ones(n), 
+	# 				solver="clarabel")
 
 
 def mv(covariance: np.ndarray, expected_returns, desired_return):
@@ -75,6 +66,7 @@ class GMVPortfolio(DynamicPortfolio):
 	def __init__(self, quotes, window=22, name="markowitz GMV portfolio"):
 		super().__init__(quotes, name)
 		self._window = window
+		self._weights = self._weights.iloc[self._window:]
 		return
 
 	def _get_covariance(self, date: str | None, start=None):