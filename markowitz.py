e8e37e640a62a3353d297df2096138c6b14985ed
Zhi <zhi.liu.lin@gmail.com>
2024-05-07 11:28:17 -0400

    implemented markowitz portfolios that minimize variance for a given return and that maximize sharpe. began implementing the portfolio that minimizes variance for a given sharp

--- markowitz.py
+++ markowitz.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-# from qpsolvers import solve_qp
-# from scipy import sparse
+from scipy.optimize import fminbound
+
 
 from .base import DynamicPortfolio
 
@@ -25,14 +25,6 @@ def gmv(covariance: np.ndarray):
 	v[n] = 1
 	return np.linalg.solve(M, v)[:-1]
 
-	# return solve_qp(sparse.csc_matrix(covariance),
-	# 				q=np.zeros( (n:=covariance.shape[0]) ), 
-	# 				A=sparse.csc_matrix(np.ones(n)), 
-	# 				b=np.ones(1), 
-	# 				lb=np.zeros(n), 
-	# 				ub=np.ones(n), 
-	# 				solver="clarabel")
-
 
 def mv(covariance: np.ndarray, expected_returns, desired_return):
 	"""
@@ -40,26 +32,66 @@ def mv(covariance: np.ndarray, expected_returns, desired_return):
 	----------
 	cov : matrix
 		a covariance matrix
+	expected_returns : vector
+	desired_return : float
 
 	Returns
 	-------
 	Vector
 	"""
-	return solve_qp(sparse.csc_matrix(covariance),
-					q=np.zeros( (n:=covariance.shape[0]) ), 
-					A=sparse.csc_matrix( np.array([np.ones(n), expected_returns]) ), 
-					b=np.array([1, desired_return]), 
-					lb=np.zeros(n), 
-					ub=np.ones(n), 
-					solver="clarabel")
+	M = np.zeros(np.add(covariance.shape, 2))
+	M[:-2, :-2] = covariance
+	M[-2, :-2] = 1
+	M[:-2, -2] = -1
+	M[-1, :-2] = expected_returns.values
+	M[:-2, -1] = -expected_returns.values
+
+	v = np.zeros(covariance.shape[0]+2)
+	v[-2] = 1
+	v[-1] = desired_return
+
+	return np.linalg.solve(M, v)[:-2]
 
 
-# def max_sharp(covariance: np.ndarray, expected_returns, risk_free=0):
-# 	def f(x):
-# 		weights = mv(covariance, expected_returns, x)
-# 		return -(expected_returns @ weights - risk_free) / (weights @ covariance @ weights)
-# 	expected_return = -fmin(f, np.mean(expected_returns))[0]
-# 	return mv(covariance, expected_returns, expected_return)
+def ms(covariance: np.ndarray, expected_returns, min_return=0., max_return=.1, tol=1E-4, maxiter:int=2**6):
+	def sharp(ret):
+		weights = mv(covariance, expected_returns, ret)
+		return -ret / (weights @ covariance @ weights)
+	ideal_ret = fminbound(sharp, min_return, max_return, xtol=tol, maxfun=maxiter)
+	return mv(covariance, expected_returns, ideal_ret), ideal_ret, -sharp(ideal_ret)
+
+
+def mvs(covariance: np.ndarray, expected_returns, desired_sharpe):
+	"""
+	Parameters
+	----------
+	cov : matrix
+		a covariance matrix
+	expected_returns : vector
+	desired_sharpe : float
+
+	Returns
+	-------
+	Vector
+	"""
+	M = np.zeros( (covariance.shape[0]+1, covariance.shape[0]+2) )
+	M[:-1, :-2] = covariance
+	M[-1, :-2] = 1
+	M[:-1, -2] = -1
+	M[:-1, -1] = -expected_returns.values
+
+	print(pd.DataFrame(covariance))
+	print(expected_returns)
+	print(pd.DataFrame(M))
+
+	return 
+
+	v = np.zeros(covariance.shape[0]+2)
+	v[-2] = 1
+	v[-1] = desired_return
+
+	return np.linalg.solve(M, v)[:-2]
+
 
 
 class GMVPortfolio(DynamicPortfolio):
@@ -67,9 +99,16 @@ class GMVPortfolio(DynamicPortfolio):
 		super().__init__(quotes, name)
 		self._window = window
 		self._weights = self._weights.iloc[self._window:]
+		self._predicted_returns = self._weights.iloc[self._window:].copy(deep=True)
 		return
 
-	def _get_covariance(self, date: str | None, start=None):
+	def _predict_returns(self, date, start=None):
+		if not start:
+			start = self._quotes.index.get_loc(date)-self._window
+		start = self._quotes.index[start]
+		return self._quotes.loc[start:date].dropna(axis=1).pct_change().mean()
+
+	def _get_covariance(self, date: str, start=None):
 		if not start:
 			start = self._quotes.index.get_loc(date)-self._window
 		start = self._quotes.index[start]
@@ -83,5 +122,61 @@ class GMVPortfolio(DynamicPortfolio):
 		weights.loc[cov.index] = gmv(cov.values) 
 		return weights
 
+	def predict_returns(self, date, start=None):
+		if pd.isna(self._predicted_returns.loc[date].iloc[0]):
+			rets = pd.Series(0., self.universe)
+			temp = self._predict_returns(date, start)
+			rets.loc[temp.index] = temp
+			self._predicted_returns.loc[date] = rets
+		return self._predicted_returns.loc[date]
+
 	def get_covariance(self, date, start=None):
 		return self._get_covariance(date, start)
+
+
+class MVPortfolio(GMVPortfolio):
+	def __init__(self, desired_return, quotes, window=22, name="markowitz MV portfolio"):
+		super().__init__(quotes, window, name)
+		self._desired_return = desired_return
+		return
+
+	def _get_weights(self, date=None):
+		weights = pd.Series(0., self.universe)
+		if (start:=self._quotes.index.get_loc(date)-self._window) < 0:
+			return weights
+		cov = self._get_covariance(date, start)
+		predicted_returns = self.predict_returns(date, start).loc[cov.index]
+		weights.loc[cov.index] = mv(cov, predicted_returns, self._desired_return)
+		return weights
+
+
+class MSPortfolio(GMVPortfolio):
+	def __init__(self, quotes, window, name="markowitz MS portfolio"):
+		super().__init__(quotes, window, name)
+		self._predicted_sharps = pd.Series(0., index=self._weights.index[self._window:])
+		return
+
+	def _get_weights(self, date=None):
+		weights = pd.Series(0., self.universe)
+		if (start:=self._quotes.index.get_loc(date)-self._window) < 0:
+			return weights
+		cov = self._get_covariance(date, start)
+		predicted_returns = self._predict_returns(date, start)
+		weights.loc[cov.index], _, self._predicted_sharps[date] = ms(cov, predicted_returns)
+		return weights
+
+
+class MVSPortfolio(GMVPortfolio):
+	def __init__(self, desired_sharpe, quotes, window=22, name="markowitze MVS portfolio"):
+		super().__init__(quotes, window, name)
+		self._desired_sharpe = desired_sharpe
+		return
+
+	def _get_weights(self, date=None):
+		weights = pd.Series(0., self.universe)
+		if (start:=self._quotes.index.get_loc(date)-self._window) < 0:
+			return weights
+		cov = self._get_covariance(date, start)
+		predicted_returns = self.predict_returns(date, start).loc[cov.index]
+		weights.loc[cov.index] = mvs(cov, predicted_returns, self._desired_sharpe)
+		return weights
